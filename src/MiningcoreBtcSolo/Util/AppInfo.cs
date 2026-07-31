1f6506ca4e0aa09c2fda57e002636f78ff8526e2
bugoao <46293847+bugoao@users.noreply.github.com>
2026-08-01 01:16:56 +0800

    Update AppInfo.cs

--- src/MiningcoreBtcSolo/Util/AppInfo.cs
+++ src/MiningcoreBtcSolo/Util/AppInfo.cs
@@ -3,7 +3,7 @@ namespace MiningcoreBtcSolo.Util;
 internal static class AppInfo
 {
     public const string Name = "miningcore-btc-solo";
-    public const string Version = "1.0.0";
+    public const string Version = "1.1.1";
     public const string ProtocolVersion = Name + "/" + Version;
     public const string DashboardVersion = Name + " " + Version;
     public const string P2pUserAgent = "/" + Name + ":" + Version + "/";
